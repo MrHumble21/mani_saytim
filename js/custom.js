c355c2a5ee605cd254a3cc2245daf90e24bf9003
= <=>
2021-12-24 11:47:34 +0500

    100 % done

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('#menu').click(function () {
         $(this).toggleClass('fa-times');
         $("header").toggleClass('toggle');
@@ -7,7 +7,25 @@ $(document).ready(function(){
         $(window).on('scroll load', function () {
             $('#menu').removeClass('fa-times');
             $("header").removeClass('toggle');
+
+
+            if($(window).scrollTop() > 0) {
+                $('.top, .btn').show();
+            }
+            else {
+                $('.top , .btn').hide();
+            }
         })
 
+
+    })
+    $('a[href*="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        $('body,html').animate({
+                scrollTop: $($(this).attr('href')).offset().top,
+            },
+            500,
+            "linear")
     })
 })
